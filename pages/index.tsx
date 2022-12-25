c9ef2e17a919a6c3922fed695832f16da859d174
ahmed <zrouqui.ahmed.az@gmail.com>
2022-12-25 14:11:07 +0100

    add google ads script

--- pages/index.tsx
+++ pages/index.tsx
@@ -42,9 +42,18 @@ export default function Index({ posts }: Props) {
             data-ad-slot="7003765774"
             data-ad-format="auto"
             data-full-width-responsive="true"></ins>
-          <script>
-            (adsbygoogle = window.adsbygoogle || []).push({ });
-          </script>
+          <Script
+            id="adsbygoogle-init"
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+                __html: `
+                (adsbygoogle = window.adsbygoogle || []).push({
+                  google_ad_client: {process.env.NEXT_PUBLIC_GOOGLE_ADSENSE},
+                  enable_page_level_ads: true
+                  });
+                `,
+              }}
+            />
           {posts.length > 0 && <MoreStories posts={posts} />}
         </Container>
       </Layout>
