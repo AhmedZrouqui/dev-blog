f337e005a9656ec84c1958435920e254224973e2
ahmed <zrouqui.ahmed.az@gmail.com>
2022-12-25 02:58:17 +0100

    add google adsense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,25 +5,26 @@ import Script from "next/script"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-  <>
-  <Script
-    strategy='lazyOnload'
-    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-  />
-  <Script
-  strategy='lazyOnload'
-  >
-    {
-          `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-        
-          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
-          ` 
-    }
-  </Script>
-  <Component {...pageProps} />
+    <>
+      <Script async src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_GOOGLE_ADSENSE}`} crossOrigin="anonymous" />
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script
+      strategy='lazyOnload'
+      >
+        {
+              `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments)}
+              gtag('js', new Date());
+            
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+              ` 
+        }
+      </Script>
+      <Component {...pageProps} />
   </>)
   
 }
