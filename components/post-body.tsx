8b626bb9386482e0ba421e9b890d7bccd93bc828
ahmed <zrouqui.ahmed.az@gmail.com>
2022-12-26 03:36:23 +0100

    Improve text readability

--- components/post-body.tsx
+++ components/post-body.tsx
@@ -15,25 +15,25 @@ const PostBody = ({ content }: Props) => {
   const options = {
     renderNode: {
       [BLOCKS.HEADING_1]: (node: Block | Inline, children: any) => (
-        <h1 className="my-4 font-bold text-5xl">{children}</h1>
+        <h1 className="my-4 font-bold text-5xl text-[#1D1D1D]">{children}</h1>
       ),
       [BLOCKS.HEADING_2]: (node: Block | Inline, children: any) => (
-        <h2 className="my-4 font-bold text-3xl">{children}</h2>
+        <h2 className="my-4 font-bold text-3xl text-[#1D1D1D]">{children}</h2>
       ),
       [BLOCKS.HEADING_3]: (node: Block | Inline, children: any) => (
-        <h3 className="my-4 font-bold text-2xl">{children}</h3>
+        <h3 className="my-4 font-bold text-2xl text-[#1D1D1D]">{children}</h3>
       ),
       [BLOCKS.HEADING_4]: (node: Block | Inline, children: any) => (
-        <h4 className="my-4 font-bold text-xl">{children}</h4>
+        <h4 className="my-4 font-bold text-xl text-[#1D1D1D]">{children}</h4>
       ),
       [BLOCKS.HEADING_5]: (node: Block | Inline, children: any) => (
-        <h5 className="my-4 font-normal text-xl">{children}</h5>
+        <h5 className="my-4 font-normal text-xl text-[#1D1D1D]">{children}</h5>
       ),
       [BLOCKS.HEADING_5]: (node: Block | Inline, children: any) => (
-        <h6 className="my-4 font-normal text-xl">{children}</h6>
+        <h6 className="my-4 font-normal text-xl text-[#1D1D1D]">{children}</h6>
       ),
       [BLOCKS.PARAGRAPH]: (node: Block | Inline, children: any) => (
-        <p className="mb-3">{children}</p>
+        <p className="mb-3 text-lg leading-relaxed font-normal text-[#1D1D1D]">{children}</p>
       ),
       [BLOCKS.EMBEDDED_ASSET]: (node: Block | Inline) => {
         return (
